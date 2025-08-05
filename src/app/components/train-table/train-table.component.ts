73351350b09e765220148b392ea2ad78bb26b008
Mariia Tyshkovets <maria15021996@gmail.com>
2025-08-05 17:44:24 +0300

    added main pages

---
+++ src/app/components/train-table/train-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-train-table',
+  imports: [],
+  templateUrl: './train-table.component.html',
+  styleUrl: './train-table.component.scss'
+})
+export class TrainTableComponent {
+
+}
